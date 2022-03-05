261203a19fb8102216a08e8b64d29fa179057018
Allan <allanfelipedk@gmail.com>
2022-03-05 09:00:47 -0300

    Physics Queries: Overlaps

--- Assets/Runtime/Scripts/Bomb.cs
+++ Assets/Runtime/Scripts/Bomb.cs
@@ -7,9 +7,18 @@ public class Bomb : MonoBehaviour
 {   
     [SerializeField] private float explosionDelay = 3;
     [SerializeField] private Color explosionColorBomb = Color.red;
+    [SerializeField] private float explosionRadius = 2;
+    [SerializeField] private float explosionForce = 100;
+    [SerializeField] private float upswardModifier = 0;
+    [SerializeField] private LayerMask explosionLayer;
+    
     [SerializeField] private ParticleSystem explosionParticles;
+
+    private Collider[] colliderInRange = new Collider[20];
     private bool isCountDown = false;
     private Renderer rend;
+    
+
     private void Awake()
     {
         rend = GetComponent<Renderer>();
@@ -34,6 +43,36 @@ public class Bomb : MonoBehaviour
             yield return null;
         }
 
+        Explode();
+    }
+    private void Explode()
+    {
+        PlayExplosionEffects();
+        var colliderCount = Physics.OverlapSphereNonAlloc(
+            transform.position,
+            explosionRadius,
+            colliderInRange,
+            explosionLayer,
+            QueryTriggerInteraction.Ignore
+        );
+
+
+        for (int i = 0; i < colliderCount; i++)
+        {
+            var collider = colliderInRange[i];
+            if(collider.TryGetComponent<Rigidbody>(out var rb))
+            {
+                // var toRB = rb.position - transform.position;
+                // var percent  = Mathf.Clamp01((explosionRadius - toRB.magnitude) / explosionRadius);
+                // var force = explosionForce * toRB.normalized * percent;
+                // rb.AddForce(force, ForceMode.Impulse);
+                rb.AddExplosionForce(explosionForce, transform.position, explosionRadius, upswardModifier, ForceMode.Impulse);
+            }
+        }
+    }
+
+    private void PlayExplosionEffects()
+    {
         explosionParticles.gameObject.SetActive(true);
         explosionParticles.transform.SetParent(null);
         explosionParticles.Play();
@@ -43,5 +82,10 @@ public class Bomb : MonoBehaviour
         Destroy(explosionParticles.gameObject, explosionFxTime);
 
         gameObject.SetActive(true);
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
     }
 }
